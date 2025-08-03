f0d5b2eb259c4570cc7816de5cfbf7ea3751e7c8
solmvz21 <cesar.bakuninn@gmail.com>
2025-08-03 20:46:28 +0300

    Update urls.py

--- shopier/urls.py
+++ shopier/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('order/', views.order_form_view, name='order_form'),
-    path('order/pay', views.shopier_payment_post, name='shopier_payment'),
+    path('order/pay/', views.shopier_payment_post, name='shopier_payment'),
     path('callback/', views.shopier_callback, name='shopier_callback'),  # Yeni eklendi
 ]
+
 
